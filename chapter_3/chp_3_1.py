aee41a3c6e7f1e5b4031475c11d12698d9e27d19
mercynary23 <mercynary23@gmail.com>
2022-10-15 22:11:30 +0100

    A few changes made

--- chapter_3/chp_3_1.py
+++ chapter_3/chp_3_1.py
@@ -2,7 +2,8 @@ print("And on this rock I shall build my church")
 
 arr_goats = ['MJ', 'Magic', 'T-Mac', 'LeBron', 'Kobe', 'Giannis']
 
-for player in arr_goats: 
-    print(player)
+for player in arr_goats:
+    message = f"Are you the GOAT: {player}?" 
+    print(message)
 
 
---
+++ chapter_3/chp_3_3.py
@@ -0,0 +1,9 @@
+#Create a list of cars; each item is a String 
+
+arr_cars = ['Mazzie', 'Benzo', 'GLC'] 
+
+for car in arr_cars:
+    msg_txt = f"I would like to own a {car}"
+    print(msg_txt)
+
+
