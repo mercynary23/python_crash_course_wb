98ce75ef3b43602b493d80179b32715b31f2fa4c
mercynary23 <mercynary23@gmail.com>
2022-10-16 23:36:37 +0100

    Updated

--- chapter_3/more_motorcycles.py
+++ chapter_3/more_motorcycles.py
@@ -1,2 +1,14 @@
 print("Good morning Vienna!")
 
+motorcycles = ['Honda', 'Yamaha', 'Suzuki', 'Ducati']
+print(motorcycles) 
+
+motorcycles.remove('Yamaha')
+print(motorcycles)
+
+too_expensive = 'Ducati' 
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+
